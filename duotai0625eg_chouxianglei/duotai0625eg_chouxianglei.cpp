38f2c8bd91a18e408b008798890ce8216f1db1d7
20172104249 <2980796841@qq.com>
2018-06-25 16:49:03 +0800

    Initial commit

---
+++ duotai0625eg_chouxianglei/duotai0625eg_chouxianglei.cpp
@@ -0,0 +1,34 @@
+// duotai0625eg_chouxianglei.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CBase
+{
+public:
+	virtual void test() = 0;
+	virtual void display()
+	{
+		cout << "基类" << endl;
+	}
+};
+class CDervid :public CBase
+{
+public:
+	void test()
+	{
+
+	}
+	void display()
+	{
+		cout << "派生类" << endl;
+	}
+};
+
+int main()
+{
+	CBase *p;
+	CDervid obj1;
+	return 0;
+}
